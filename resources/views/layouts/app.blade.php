b30df26c83afe188cf8f1938a734235303fdb44e
vadelgado <vadelgado@udenar.edu.co>
2023-06-17 21:50:10 -0500

    17/06/2023 10 pm @vadelgado implementacion Fronend Navbar, sidebar validacion que se comporten correctamente si se esta logueado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,33 +12,42 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="{{ mix('resources\css\app.css') }}">
 
-    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
-		
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="{{asset('css/main.css')}}">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Iconos de Bootstrap -->
+    
+
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+
+   <!-- Navbar--> 
+     <div id="app">
+            <nav class="navbar navbar-expand-md navbar-light">
+            <div class=" container-fluid">
+             <!--   <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
-                </a>
+                </a>-->
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    @auth
-                    <ul class="navbar-nav me-auto">
-                    <a class="nav-link" href="{{ route('usuarios.index') }}">{{ __('usuarios') }}</a>
+                    <ul class="navbar-nav d-flex justify-content-center align-items-center">
+                        <a class="navbar-botonmenu" href="#">
+                            <i class="bi bi-list"></i>
+                        </a>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#"><b>Institucion Educativa Siglo XX</b></a>
+                        </li>
                     </ul>
-                    @endauth
+
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
@@ -55,17 +64,20 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
+                            <li class="nav-item dropdown ">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                <div class=" dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink" id="muenudrop">
+                                <a class="dropdown-item" href="#">Perfil</a>
+                                <a class="dropdown-item" href="#">Configuración</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                        {{ __('Cerrar sesión') }}
+                                </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
@@ -77,10 +89,66 @@
                 </div>
             </div>
         </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
     </div>
+
+    @auth
+<!-- Sidebar -->
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-3 col-lg-2 bg-blue">
+            <!-- Contenido de la barra lateral -->
+
+             <!-- Contenido de la barra lateral -->
+             <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                <a href="" class="imagen-con-lineas">
+                <img src="{{ asset('storage/images/logo.webp') }}" alt="Imagen 1" class="imagen-pequena"> 
+                </a>
+                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                    <li class="nav-item ">
+                        <a href="#" class="nav-link align-middle px-0">
+                        <img src="{{ asset('storage\images\Inicio.png') }}" alt="Inicio" class="icono-sidebar"><span class="ms-1 d-none d-sm-inline ">Inicio</span>
+                        </a>
+                    </li>
+                    <li class="nav-item ">
+                        <a href="#" class="nav-link align-middle px-0">
+                        <img src="{{ asset('storage\images\usuario.png') }}" alt="usuario" class="icono-sidebar"> <span class="ms-1 d-none d-sm-inline ">Usuario</span>
+                        </a>
+                    </li>
+                    <li class="nav-item ">
+                        <a href="#" class="nav-link align-middle px-0">
+                        <img src="{{ asset('storage\images\matricula.png') }}" alt="matricula" class="icono-sidebar"> <span class="ms-1 d-none d-sm-inline ">Matricula</span>
+                        </a>
+                    </li>
+                    <li class="nav-item ">
+                        <a href="#" class="nav-link align-middle px-0">
+                        <img src="{{ asset('storage\images\notas.png') }}" alt="notas" class="icono-sidebar"> <span class="ms-1 d-none d-sm-inline vertical-center">Notas</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        @endauth
+
+        <div class="col-md-9 col-lg-10">
+            <div class="row">
+                <div class="col mt-4">
+                    <!-- Contenido principal de la página -->
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+
+
+
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 Route::resource('usuarios', App\Http\Controllers\UsuarioController::class)->middleware('auth');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
